7ebb475d12e78429a7650d151a8ca2c2f730e881
Adolfo Grego Micha <26657028+adolfogrego@users.noreply.github.com>
2026-06-18 12:25:31 -0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,32 +1,25 @@
-import { NextResponse } from 'next/server'
+export default function middleware(request) {
+  const authorization = request.headers.get('authorization');
 
-export function middleware(req) {
-  const basicAuth = req.headers.get('authorization')
+  if (authorization) {
+    // Rompemos el texto encriptado que manda el navegador
+    const authValue = authorization.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
 
-  if (basicAuth) {
-    // Tomamos la parte encriptada (quitando la palabra "Basic")
-    const authValue = basicAuth.split(' ')[1]
-    
-    // Desencriptamos el texto para obtener el usuario y la clave
-    const decoded = atob(authValue).split(':')
-    const pwd = decoded[1]
-
-    // CAMBIA TU CONTRASEÑA AQUÍ:
+    // CAMBIA TU CONTRASEÑA AQUÍ (El usuario puede ser cualquiera):
     if (pwd === 'ORANTE') {
-      return NextResponse.next()
+      // Si la contraseña es correcta, permite el paso
+      return new Response(null, {
+        headers: { 'x-middleware-next': '1' }
+      });
     }
   }
 
-  // Si no hay contraseña o es incorrecta, bloquea la pantalla
-  return new NextResponse('Se requiere autenticación', {
+  // Si no hay clave, lanza la ventana flotante del navegador
+  return new Response('Se requiere autenticación', {
     status: 401,
     headers: {
-      'WWW-Authenticate': 'Basic realm="Zona Protegida"',
-    },
-  })
-}
-
-// Configuración para proteger todo el sitio web
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+      'WWW-Authenticate': 'Basic realm="Zona Protegida"'
+    }
+  });
 }
