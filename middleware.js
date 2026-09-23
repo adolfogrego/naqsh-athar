3609d2fc099795b33a2d0b362e96830de364a6f7
Adolfo Grego Micha <26657028+adolfogrego@users.noreply.github.com>
2026-09-22 21:41:09 -0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,28 +1,6 @@
 export default function middleware(request) {
-  const authorization = request.headers.get('authorization');
-
-  if (authorization) {
-    const authValue = authorization.split(' ');
-    
-    // Desencriptamos los datos del cuadro de diálogo
-    const decoded = atob(authValue[1]).split(':');
-    
-    // El segundo elemento [1] es siempre la contraseña
-    const pwd = decoded[1];
-
-    // COLOCA TU CONTRASEÑA AQUÍ:
-    if (pwd === 'ORANTE') {
-      return new Response(null, {
-        headers: { 'x-middleware-next': '1' }
-      });
-    }
-  }
-
-  // Si no hay contraseña o es incorrecta, vuelve a pedirla
-  return new Response('Se requiere autenticación', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Zona Protegida. Pon solo la contraseña y deja el usuario vacío."'
-    }
+  // Otorga acceso público inmediato a cualquier petición
+  return new Response(null, {
+    headers: { 'x-middleware-next': '1' }
   });
 }
