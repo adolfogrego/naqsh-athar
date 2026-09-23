2735bdd3b6c95cd7f2f98f1879f33cac6ad354c3
Adolfo Grego Micha <26657028+adolfogrego@users.noreply.github.com>
2026-09-22 21:45:36 -0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,1 @@
-export default function middleware(request) {
-  // Otorga acceso público inmediato a cualquier petición
-  return new Response(null, {
-    headers: { 'x-middleware-next': '1' }
-  });
-}
+
